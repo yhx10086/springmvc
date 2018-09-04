df40a1350bc36468f10b40cf6aa411f2766d0919
yhx <1402460488@qq.com>
2018-09-04 11:37:12 +0800

    @RequestParam

--- src/main/java/cap/controller/HelloController.java
+++ src/main/java/cap/controller/HelloController.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 @Controller
@@ -14,7 +16,7 @@ public class HelloController {
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public ModelAndView test() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
+        modelAndView.setViewName("test");
         return modelAndView;
     }
 
@@ -44,4 +46,19 @@ public class HelloController {
         map.put("key2", "value2");
         return map;
     }
+
+    @RequestMapping(value = "sayHi", method = RequestMethod.POST)
+    public String hello(HttpServletRequest request, Model model, @RequestParam("name") String username) {
+        String nameByRequest = request.getParameter("name");
+        model.addAttribute("nameByReuest", nameByRequest);
+        model.addAttribute("username", username);
+        return "result";
+
+    }
+
+    @RequestMapping(value = "/sayHi", method = RequestMethod.GET)
+    public String hello() {
+        return "sayHi";
+    }
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,5 +5,7 @@
 <a href="hello/test2">test2</a>
 <a href="hello/test3">test3</a>
 <a href="hello/test4">test4</a>
+<a href="hello/sayHi">访问表单</a>
+<a href="hello/result">访问表单1</a>
 </body>
 </html>
